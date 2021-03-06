bf880ca59cabda0c508c986b40a34e90df1fb43c
MariaEripete <mariaichor@gmail.com>
2021-03-06 10:11:47 +0300

    add the on click element

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $("#design-img").click(function(){
       $("#design-img").slideDown('1500').hide('1000');
       $("#design").show('1500');
@@ -84,12 +84,14 @@ $(document).ready(function(){
     }).mouseout(function(){
       $("#overlay8").hide();
     });
-    $("foms").submit(function(event){
+    $("foms").submit(function(){
         // event.preventDefault();
         var name = $("input#name").val();
         var email = $("input#email").val();
         var message = $("textarea#comment").val();
-        if ($("input#name").val() && $("input#email").val()){
+        //Name == '' || Email == '' || Message == ''
+        //"input#name").val() && $("input#email").val()
+        if ($(name == '' || email == '' || message == ''){
           alert (name + ", we have received your message. Thank you for reaching out to us.");
         }
         else {
@@ -98,4 +100,48 @@ $(document).ready(function(){
         
       });
     
+  });*/
+
+
+
+
+
+
+  $(document).ready(function() {
+
+    $("#design").click(function(){
+      $("#design-showing").toggle();
+      $(".hide-design-image").toggle();
+    })
+  
+    $("#development").click(function(){
+      $("#development-showing").toggle();
+      $(".hide-development-image").toggle();
+    })
+  
+    $("#productManagement").click(function(){
+      $("#product-management-showing").toggle();
+      $(".hide-product-management").toggle();
+    })
+  
+    //Insert white box upo hover
+    $(".Hover").hover(function(){
+      $(this).append({opacity:'2'});
+    },
+    function(){
+      $(this).append({opacity:'0'});
+    })
+    
+  $('.submit').click(function () {
+          var Name = $('#mce-FNAME').val();
+          var Email = $('#mce-EMAIL').val();
+          var Message = $('#mce-MESSAGE');
+          
+          if (Name == '' || Email == '' || Message == '') {
+              alert('Please make sure you have filled in the form correctly!');
+          } else {
+              alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
+          }
+    });
   });
+  
